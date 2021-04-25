2fe8075fa01dfee33bbd982aaf5445a86b53956f
abhinav sharma <abhiut37@gmail.com>
2021-04-25 22:12:37 +0530

    made a template class

--- templateClassesInCpp.cpp
+++ templateClassesInCpp.cpp
@@ -1,3 +1,49 @@
-#include <stdio.h>
-#include <iostream>
+#include<stdio.h>
+#include<iostream>
 
+using namespace std;
+
+//for writing tmplate class
+template <class T>
+class Calculations{
+  private:
+    T length;
+    T bredth;
+  public:
+    Calculations(T a, T b);
+    T add();
+    T muliply();
+    void changeLength(T len);
+    void changeBreadh(T bre);
+};
+//if you want to declare the functions outside the class
+template<class T>
+Calculations<T>::Calculations(T a, T b){
+  this->length = a;
+  this->bredth = b;
+};
+template<class T>
+T Calculations<T>::add(){
+  return length + bredth;
+};
+
+template<class T>
+T Calculations<T>::muliply(){
+  return length * bredth;
+};
+
+template<class T>
+void Calculations<T>::changeLength(T len) {
+  this->length = len;
+};
+
+template<class T>
+void Calculations<T>::changeBreadh(T bre){
+  this->bredth = bre;
+};
+int main(){
+  Calculations<float> cal(2.5,45.56),cal2(10,10);
+  cout << cal.add() << endl;
+  cout << cal2.add() << endl;
+
+}
