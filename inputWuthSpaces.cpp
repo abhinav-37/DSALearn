5bdf952c6350e300c68101b23ba9914df9b65a30
abhinav sharma <abhiut37@gmail.com>
2021-04-27 15:57:49 +0530

    done with the recurtions

---
+++ inputWuthSpaces.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main(){
+  int arr[5];
+  cout << "enter ssome numbers";
+  for (int i = 0; i < 5; i++)
+  {
+    cin >> arr[i];
+  };
+
+  cout << arr[4];
+
+  return 0;
+}
