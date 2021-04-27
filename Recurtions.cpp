43d34417d56822d9fb6a3e34efa20d68e43330f5
abhinav sharma <abhiut37@gmail.com>
2021-04-27 16:15:17 +0530

    did some changes

--- Recurtions.cpp
+++ Recurtions.cpp
@@ -1,16 +1,17 @@
 #include<iostream>
-
+#include <chrono>
 using namespace std;
 
 void fun1(int n){
   if(n>0){
-    fun1(n - 1);
     cout << n << endl;
+    fun1(n - 1);
+   
   }
 }
 
 int main(){
-  int a = 4;
+  int a = 100;
   fun1(a);
   return 0;
 }
